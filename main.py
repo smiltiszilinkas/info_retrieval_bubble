62497debc6a4762b5302c95d2f50c233064912c5
smiltiszilinskas <smiltis2000@gmail.com>
2024-11-27 17:56:43 +0100

    navigating between queries and clicking top 4 links, excluding socials and saving neutral queries to json

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException  # Import the exception
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 import json
 import random
+import os
 
-def get_queries():
+excluded_domains = ['wikipedia', 'instagram.com', 'facebook.com', "x.com", "twitter.com" ]
+nr_of_links_clikable = 4
+
+def get_queries(name):
     """Load the queries from queries.json."""
     with open('queries.json', 'r') as file:
         data = json.load(file)
-        return data.get("queries", [])
+        return data.get(name, [])
 
 def initialize_driver():
     """Initialize and configure the Chrome WebDriver."""
@@ -46,47 +50,48 @@ def accept_cookies(driver):
 def search_queries(driver, queries):
     """Search each query in the Google search field."""
     # Locate the search field (first <textarea> element)
-    try:
-        search_field = driver.find_element(By.TAG_NAME, "textarea")
-        
+    try:        
         for index, query in enumerate(queries):
-            if index == 0: 
-                print(str(index)+" ", query)
-                search_field.click()
-                search_field.clear()  # Clear any previous text
-                search_field.send_keys(query)
-                search_field.send_keys(Keys.ENTER)
-                time.sleep(random.uniform(5, 10))  # Wait randomly for not getting banned for crawling
-                top_stories_divs = locate_top_stories_divs(driver)
-                links = []
-                # Iterate through each div and look for the <a> tag inside it
-                for div in top_stories_divs:
-                    # Find all <a> tags inside the current <div>
-                    a_tags = div.find_elements(By.TAG_NAME, "a")
+            driver.get("https://www.google.com/?hl=en&gl=us")
+            search_field = driver.find_element(By.TAG_NAME, "textarea")
+            print(str(index)+" ", query)
+            search_field.click()
+            search_field.clear()  # Clear any previous text
+            search_field.send_keys(query)
+            search_field.send_keys(Keys.ENTER)
+            time.sleep(random.uniform(5, 10))  # Wait randomly for not getting banned for crawling
+            search_query_divs = locate_query_divs(driver)
+            links = []
+            # Iterate through each div and look for the <a> tag inside it
+            for div in search_query_divs:
+                # Find all <a> tags inside the current <div>
+                a_tags = div.find_elements(By.TAG_NAME, "a")
 
-                    # Check if any <a> tags were found
-                    if a_tags:
-                        for a_tag in a_tags:
-                            link = a_tag.get_attribute("href")
-                            text = a_tag.text
+                # Check if any <a> tags were found
+                if a_tags:
+                    for a_tag in a_tags:
+                        link = a_tag.get_attribute("href")
+                        text = a_tag.text
+                        if not any(domain in link for domain in excluded_domains): 
                             print("Found <a> tag with href:", link)
                             print("Text inside <a> tag:", text)
                             links.append({"link": link, "name": text})
-                    else:
-                        print("No <a> tags found inside this div.")      
-                for link in links:
+                else:
+                    print("No <a> tags found inside this div.")
+            links_clicked = 0      
+            for index in range(len(links)):
+                link = links[index]
+                if links_clicked < nr_of_links_clikable: 
+                    links_clicked +=1
                     clickLink(driver, link)       
-            else:
-                time.sleep(random.uniform(500, 1000))  # Wait randomly between searches for realism
-                print("Else")
     except NoSuchElementException:
         print("Search field not found.")
     except ElementNotInteractableException:
         print("Search field is not interactable.")
 
-def locate_top_stories_divs(driver):
+def locate_query_divs(driver):
     """
-    Locate the parent div five levels up from the 'Top stories' span. Returns divs 0 - main news, 1 also in the news, 2- more news
+    Locates the divs of search query. Search results divs.
     
     Returns:
         WebElement or None
@@ -94,31 +99,92 @@ def locate_top_stories_divs(driver):
     try:
         # Use XPath to find the span with exact text 'Top stories'
         top_stories_span = driver.find_element(By.XPATH, "//span[text()='Top stories']")
-        # Navigate up five ancestor divs
-        top_stories_parent = top_stories_span.find_element(By.XPATH, "./ancestor::div[5]")
-         # Find all divs within top_stories_parent
-        divs_within_top_stories = top_stories_parent.find_elements(By.XPATH, "./div")
-        # Check if there is a second div within top_stories_parent
-        if len(divs_within_top_stories) >= 2:
-            second_div = divs_within_top_stories[1]
-            g_section_element = second_div.find_element(By.XPATH, "./*")
-            g_stories_divs = g_section_element.find_elements(By.XPATH, "./div")
+        # Navigate up 8 ancestor divs
+        parent = top_stories_span.find_element(By.XPATH, "./ancestor::div[8]")
+        # Find all divs of search query, first one represents 'Top stories'
+        divs = parent.find_elements(By.XPATH, "./div")
 
-            
-            return g_stories_divs # 0 div main news, 1 div  also in the news, 2 more news
-        else:
-            print("The second div inside 'top_stories_parent' was not found.")
-            return None
+        return divs[1:]
 
     except NoSuchElementException:
-        print("'Top stories' span or its ancestor div not found.")
+        print("Divs for this query have not been found.")
         return None
+
+def save_links_to_json(links):
+    """
+    Saves links in the json
+    """
+     
+    # Directory to save the JSON files
+    output_dir = "neutral_queries_jsons"
+    os.makedirs(output_dir, exist_ok=True)  # Ensure the directory exists
+
+    # filename
+    base_filename = "links"
+    extension = ".json"
+    counter = 0
+
+    # Find filename, if doesn't exist create one
+    while True:
+        if counter == 0:
+            output_file = os.path.join(output_dir, f"{base_filename}{extension}")
+        else:
+            output_file = os.path.join(output_dir, f"{base_filename}{counter}{extension}")
+        if not os.path.exists(output_file):
+            break
+        counter += 1
+
+    # Save the links to the unique JSON file
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(links, f, ensure_ascii=False, indent=4)
+
+    print(f"Saved {len(links)} links to {output_file}")
+
+def search_query_save_results(driver, neutral_queries):
+    """Search each query in the Google search field. And save links to json."""
+    # array to store links
+    array = []
+    try:        
+        for index, query in enumerate(neutral_queries):
+            driver.get("https://www.google.com/?hl=en&gl=us")
+            search_field = driver.find_element(By.TAG_NAME, "textarea")
+            search_field.click()
+            search_field.clear()  # Clear any previous text
+            search_field.send_keys(query)
+            search_field.send_keys(Keys.ENTER)
+            time.sleep(random.uniform(5, 10))  # Wait randomly for not getting banned for crawling
+            search_query_divs = locate_query_divs(driver)
+            links = []
+            # Iterate through each div and look for the <a> tag inside it
+            for div in search_query_divs:
+                # Find all <a> tags inside the current <div>
+                a_tags = div.find_elements(By.TAG_NAME, "a")
+
+                # Check if any <a> tags were found
+                if a_tags:
+                    for a_tag in a_tags:
+                        link = a_tag.get_attribute("href")
+                        text = a_tag.text
+                        if not any(domain in link for domain in excluded_domains):
+                            links.append({"link": link, "name": text})
+            array.append(links)
+
+        # save neutral links to json, only single for now
+        save_links_to_json(array)
+            
+
+    except NoSuchElementException:
+        print("Search field not found.")
+    except ElementNotInteractableException:
+        print("Search field is not interactable.")
 
 def clickLink(driver, link):
     """
     Function to click a link, wait 10 seconds, navigate back, and repeat.
     """
     try:
+        #save current path 
+        current_url = driver.current_url
         # Open the link
         driver.get(link['link'])
         
@@ -128,28 +194,31 @@ def clickLink(driver, link):
         # Wait for 10 seconds
         time.sleep(10)
         
-        # Navigate back to the previous page
-        driver.back()
+        # Navigate back to the previous search page
+        driver.get(current_url)
         
         # Wait for the page to load after navigating back
-        time.sleep(2)  # Adjust as needed based on your website's load time
+        time.sleep(2)
     
     except Exception as e:
         print(f"Error occurred while handling link {link['link']}: {e}")
 
 def main():
-    queries = get_queries()
+    queries = get_queries('queries')
     
     driver = initialize_driver()
     
     # Navigate to Google with English settings - hl=en: Sets the language of the Google interface to English. gl=us: Sets the region to the United States as we want english results and be focused on US politics.
     driver.get("https://www.google.com/?hl=en&gl=us")
-    
     accept_cookies(driver)
     
     # Perform searches for each query
     search_queries(driver, queries)
     
+    #get neutral queries and save to the json
+    neutral_queries = get_queries('neutral_queries')
+    search_query_save_results(driver, neutral_queries)
+
     # Close the browser
     driver.quit()
 
